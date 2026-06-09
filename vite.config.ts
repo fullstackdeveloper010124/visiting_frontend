b564df38a252489aee9c954fad7c42b001bd7de2
Md. Imran Molla <fullstackdeveloper010124@gmail.com>
2026-06-09 15:26:15 +0530

    update on 09-06-2026
    update on 09-06-2026

--- vite.config.ts
+++ vite.config.ts
@@ -15,7 +15,7 @@ export default defineConfig({
     },
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
+        target: 'https://visiting-backend.onrender.com',
         changeOrigin: true,
       },
     },
